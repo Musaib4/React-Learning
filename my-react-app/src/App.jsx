7f40bf2c7c353caa05c1b0ee15c83fb4205535da
Musaib Misger <musaibmisger0@gmail.com>
2025-12-07 13:59:18 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './header.jsx'
+import Footer from './footer.jsx'
+import Card from './card.jsx'
+
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Card></Card>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/card.jsx
@@ -0,0 +1,20 @@
+function Card() {
+  const style = {
+    background: "lightgray",
+    padding: "10px",
+    borderRadius: "8px",
+    marginTop: "20px",
+    marginBottom: "20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  };
+
+  return (
+    <>
+      <div style={style}>this is Card</div>
+    </>
+  );
+}
+
+export default Card;
